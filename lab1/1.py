3cbfdf1a6a856bfde99c39f7d5bb5a210f86cd8b
AdskiyDrochila369 <trollaba3000@gmail.com>
2026-09-25 01:09:48 +1000

    feat: start lab 1

---
+++ lab1/1.py
@@ -0,0 +1,8 @@
+station_name = "Ocean"
+active_sensors = 18
+average_temperature =21.6
+is_online = True
+print("Ocean", type(station_name))
+print(18, type(active_sensors))
+print(21.6, type(average_temperature))
+print(True, type(is_online))
